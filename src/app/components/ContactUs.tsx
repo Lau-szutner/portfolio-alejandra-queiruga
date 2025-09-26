c0ceb8338d3f19ea751aa2f5a96726e69588ca03
lau-szutner <lautaroszutner@gmail.com>
2025-09-25 22:15:39 -0300

    termino texto

--- src/app/components/ContactUs.tsx
+++ src/app/components/ContactUs.tsx
@@ -11,20 +11,20 @@ const ContactUs = () => {
       text: 'Asesoría y defensa inmediata en casos de detención o citaciones judiciales. Acompañamos al cliente desde el primer momento para garantizar sus derechos.',
     },
     {
-      heading: 'Detenciones y causas penales',
+      heading: 'Homicidios culposos o dolosos',
       text: 'Asesoría y defensa inmediata en casos de detención o citaciones judiciales. Acompañamos al cliente desde el primer momento para garantizar sus derechos.',
     },
     {
-      heading: 'Detenciones y causas penales',
-      text: 'Asesoría y defensa inmediata en casos de detención o citaciones judiciales. Acompañamos al cliente desde el primer momento para garantizar sus derechos.',
+      heading: 'Delitos vinculados a drogas',
+      text: 'Intervención en procesos por tenencia, transporte o comercialización de estupefacientes. Estrategias de defensa adaptadas a cada caso.',
     },
     {
-      heading: 'Detenciones y causas penales',
-      text: 'Asesoría y defensa inmediata en casos de detención o citaciones judiciales. Acompañamos al cliente desde el primer momento para garantizar sus derechos.',
+      heading: 'Violencia y lesiones',
+      text: 'Asistencia en causas de lesiones leves o graves, violencia en conflictos personales, familiares o en el ámbito público.',
     },
     {
-      heading: 'Detenciones y causas penales',
-      text: 'Asesoría y defensa inmediata en casos de detención o citaciones judiciales. Acompañamos al cliente desde el primer momento para garantizar sus derechos.',
+      heading: 'Amenazas y extorsiones',
+      text: 'Acompañamiento legal en denuncias por amenazas, coacciones o extorsiones que afectan la libertad y tranquilidad de las personas.',
     },
   ];
 
